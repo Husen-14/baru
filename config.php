dd72f3b89114db2093e112e7083a29eb3c64f831
Husen-14 <83211972+Husen-14@users.noreply.github.com>
2021-04-30 08:12:30 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+//Jangan Lupa Subrek Ngap
+//Gw Jadi Ngeri anjing
+
+$UA = $UA = ''Mozilla/5.0 (Linux; Android 6.0.1; Redmi 3S) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.66 Mobile Safari/537.36'';
+
+
+//Cookie BTT
+$BTT = "xxxxxxxxxxxx";
+
+//Cookie TRX
+$TRX = "xxxxxxxxxxxx";
+
+//Cookie DOGE
+$DOGE = DOGEToken=UFOGf05JfUTVA9YyCT98I1uuDRO3kgcN;__cfduid=dd8d813ee960747f124410e17caba9c771619577829;PHPSESSID=e8f4a8f1d81f19f25aa74d1d03a2a2d6
+
+//Cookie LTC
+$LTC = "xxxxxxxxxxxxx";
+
+
